f5732733b063ada8387a211967842e2c280714c5
Vasanth Pandiarajan <itsmevasanth@hotmail.com>
2017-11-27 19:57:03 -0600

    Fixed syntax

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,16 @@
 class ReviewsController < ApiController
   before_action :authenticate_user
 
-	def create
-		@review = Review.create!(ReviewParameters.new(params).permit)
-		render json: @review
-	end
+  def create
+    @review = Review.create!(ReviewParameters.new(params).permit)
+    render json: @review
+  end
 
 	def index
 		@review = params[:tutor_id].present? ? User.find_by_id(params[:tutor_id]).tutor_reviews : current_user.learner_reviews
 
 		render json: @review
-	endrais
+	end
 
 	def show
 		@review = Review.find_by_id(params[:id])
