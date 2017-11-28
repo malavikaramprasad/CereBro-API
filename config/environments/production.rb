f62e73745e01033d8beef8464553d2037c129fcd
Vasanth <itsmevasanth@hotmail.com>
2017-11-27 20:27:16 -0600

    Production.rb changed

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -90,9 +90,9 @@ Rails.application.configure do
     address:               'smtp.sendgrid.net',
     port:                  '587',
     authentication:        :plain,
-    user_name:             ENV[SENDGRID_USERNAME],
-    password:              ENV[SENDGRID_PASSWORD],
-    domain:                ENV[REMOTE_HOST],
+    user_name:             ENV['SENDGRID_USERNAME'],
+    password:              ENV['SENDGRID_PASSWORD'],
+    domain:                ENV['REMOTE_HOST'],
     enable_starttls_auto:  true
   }
 
