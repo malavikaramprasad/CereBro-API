a432c68dbbcba1aabba726aa4552d83bc0331b7c
Vasanth <itsmevasanth@hotmail.com>
2017-11-27 22:44:29 -0600

    Merge issues fixed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171115041532) do
+ActiveRecord::Schema.define(version: 20171128014501) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
