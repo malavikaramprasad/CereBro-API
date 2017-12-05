52e16f097da3115443bf43281915065e8de42ead
Vasanth <itsmevasanth@hotmail.com>
2017-12-05 03:58:07 -0600

    Commit message

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,7 +2,7 @@ class UserSerializer < BaseSerializer
   attributes :first_name, :last_name, :email, :rating, :x_coordinate, :y_coordinate, :start_time, :end_time
   has_many :booked_slots, serializer: QuestionSlotSerializer
 
-  def booked_slots
-    object.tutor_questions.where("start_time > ?", DateTime.now)
-  end
+  # def booked_slots
+  #   object.tutor_questions.where("start_time > ?", DateTime.now)
+  # end
 end
